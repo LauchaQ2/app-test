bfbc3e10ee02f4d832ccda22076e242a9a807fdb
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2022-07-21 18:21:08 -0300

    add post method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -7,28 +6,65 @@ function App() {
 
   const [users, setUsers] = useState([])
 
+  const getUsers = async() =>{
+    const res =  await axios.get('https://restserver-lautaro-quevedo.herokuapp.com/api/users?limit=25')
+    setUsers(res.data.users)
+  }
+
   useEffect(() => {
-    const getUsers = async() =>{
-      const res =  await axios.get('https://restserver-lautaro-quevedo.herokuapp.com/api/users?limit=10')
-      setUsers(res.data.users)
-    }
     getUsers()
     }, []);
-  
-    console.log(users)
 
-  return (
+    const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      password: '',
+      role: ''
+  })
+    const handleChange = (e) => {
+      const {name, value} = e.target
+      setFormData({...formData, [name] : value})
+  } 
+
+
+     const handleSubmitPost = (event) =>{
+      event.preventDefault();
+      console.log(formData)
+      axios.post('https://restserver-lautaro-quevedo.herokuapp.com/api/users', formData)
+      .then(res=>{
+        console.log(res)
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+     }
+     
+     return (
     <div className="App">
+        <h1>LISTA DE USUARIOS</h1>
     {users.length === 0 && <p>Nada</p>}
     <ul>{users.map((user, i)=>{
       return(
-        <>
-        <li key={i}>{user.name}</li>
-        <li>{user.email}</li>
-        </>
+        <div className='list'>
+        <li>Usuario n°: {i}</li>
+        <li key={i}>Nombre: {user.name}</li>
+        <li>Email: {user.email}</li>
+        </div>
         )
     })}
+    <button onClick={()=>{getUsers()}}>Actualizar lista de usuarios</button>
     </ul>
+    <div className='containerform'>
+    <h1>CREAR USUARIO</h1>
+    <form className='form' onSubmit={handleSubmitPost}>
+      <input name='name' placeholder='name' onChange={handleChange}/>
+      <input name='email' placeholder='email' onChange={handleChange}/>
+      <input name='password' placeholder='password' onChange={handleChange}/>
+      <input name='role' placeholder='role' onChange={handleChange}/>
+      <button>Enviar</button>
+    </form>
+
+    </div>
     </div>
   );
 }
