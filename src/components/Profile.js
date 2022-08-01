82acae147c00e10bccc4df4caef6afa5923001bd
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2022-08-01 18:54:51 -0300

    some

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import UserContext from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import './Profile.css';
 
 const Profile = () => {
 
-    const { userLogged, setUserLogged, setIsLogin, onChangePicture,updateProfileImg} = useContext(UserContext);
+    const { userLogged, setUserLogged, setIsLogin, onChangePicture,refresh, setRefresh,update,someUp} = useContext(UserContext);
     let navigate = useNavigate();
+
+    useEffect(() => {
+        if(refresh === true){
+          navigate('/profile')
+          setRefresh(false)
+          }
+      }, [userLogged]);
 
     const clear = () => {
         setUserLogged({});
@@ -18,14 +25,17 @@ const Profile = () => {
     return (
         <div className='center'><div className='profile'>
             <div className='box-left'>
-                <img src={!userLogged.user.img ? 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' : userLogged.user.img} />
-                <input onChange={onChangePicture} name='Edit profile image' className='select-file' type="file"/>
-                <button onClick={updateProfileImg}>Actualizar imagen</button>
+                <img src={!userLogged.user.img ? 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' : userLogged.user.img || userLogged.img} />
+                <form className='form-picture' onSubmit={update}>
+                <label className={someUp ? 'label-upload2' : 'label-upload'} for='upload'>Edit image</label>
+                <input onChange={onChangePicture} id='upload' className='select-file' type="file"/>
+                <button>Actualizar imagen</button>
+                </form>
             </div>
 
             <div className='box-right'>
-                <h1>{userLogged.user.name}</h1>
-                <h3>{userLogged.user.email}</h3>
+                <h1>{userLogged.user.name || userLogged.name}</h1>
+                <h3>{userLogged.user.email || userLogged.email}</h3>
                 <h3></h3>
             </div>
         </div>
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,127 +1,157 @@
 import { createContext, useState } from "react";
 import axios from 'axios';
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 const UserContext = createContext();
 
-const UserProvider = ({children}) => {
-    const [userLogged, setUserLogged] = useState({})
-    const [isLogin, setIsLogin] = useState(false)
-    const [img, setImg] = useState({})
-    const[idUser, setIdUser] = useState()
-    const [user, setUser] = useState({
-      email: '',
-      password: ''
-    })
-    const [formData, setFormData] = useState({
-      email: '',
-      password: '',
-      name: '',
-      role: 'USER_ROLE'
-    })
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setUser({ ...user, [name]: value })
-      }
-      const handleChangeForm = (e) => {
-        const { name, value } = e.target
-        setFormData({ ...formData, [name]: value })
-      }
+const UserProvider = ({ children }) => {
+  const [userLogged, setUserLogged] = useState({})
+  const [isLogin, setIsLogin] = useState(false)
+  const [img, setImg] = useState({})
+  const [someUp, setSomeUp] = useState(false)
+  const [idUser, setIdUser] = useState()
+  const [refresh, setRefresh] = useState(false);
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    name: '',
+    role: 'USER_ROLE'
+  })
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+  }
+  const handleChangeForm = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
 
-    const login = () => {
-        console.log(user)
-          axios.post('https://restserver-lautaro-quevedo.herokuapp.com/api/auth/login', user)
-          .then(response => {
-            const data = {...response.data}
-            setUserLogged(data)
-            console.log(userLogged)
-            const id = data.user.uid; 
-            setIdUser(id)
-            setIsLogin(true)
-            console.log(isLogin)
-            console.log(data.user.uid)
-            console.log(idUser)
-          })
-          .catch(err => {
-            console.log(err.response.data.errors)
-            let er = err.response.data.errors;
-            if (er.lenght === 1) {
-              alert(er)
-            } er.map(error => {
-              alert(error.msg)
-            })
-          })
-      }
+  const login = () => {
+    console.log(user)
+    axios.post('https://restserver-lautaro-quevedo.herokuapp.com/api/auth/login', user)
+      .then(response => {
+        const data = { ...response.data }
+        setUserLogged(data)
+        console.log(userLogged)
+        const id = data.user.uid;
+        setIdUser(id)
+        setIsLogin(true)
+        console.log(isLogin)
+        console.log(data.user.uid)
+        console.log(idUser)
+      })
+      .catch(err => {
+        console.log(err.response.data.errors)
+        let er = err.response.data.errors;
+        if (er.lenght === 1) {
+          alert(er)
+        } er.map(error => {
+          alert(error.msg)
+        })
+      })
+  }
 
-      const onChangePicture = (e) => {
-        console.log(img);
-        const image = e.target.files[0];
-        setImg(image);
-    };
+  const onChangePicture = (e) => {
+    console.log(img);
+    const image = e.target.files[0];
+    setImg(image);
+    setSomeUp(true)
+  };
 
-
-      const updateProfileImg = () => {
-        console.log(img)
-
-          axios.put(`https://restserver-lautaro-quevedo.herokuapp.com/api/uploads/users/${idUser}`, img)
-          .then(response => {
-            console.log(response)
-          })
-          .catch(err => {
-            console.log(err.response.data.errors)
-            let er = err.response.data.errors;
-            if (er.lenght === 1) {
-              alert(er)
-            } er.map(error => {
-              alert(error.msg)
-            })
-          })
-      }
-
-
-      const createUser = (event) => {
-        event.preventDefault();
-        console.log(formData)
-        axios.post('https://restserver-lautaro-quevedo.herokuapp.com/api/users', formData)
-          .then(response => {
-            const data = {...response.data}
-            setUserLogged(data)
-            console.log(userLogged)
-            setIsLogin(true)
-            console.log(isLogin)
-          })
-          .catch(err => {
-            console.log(err.response.data.errors)
-            let er = err.response.data.errors;
-            if (er.lenght === 1) {
-              alert(er)
-            } er.map(error => {
-              alert(error.msg)
-            })
-          })
-      }
-
-
-    const data = {
-        login,
-        handleChange,
-        userLogged,
-        isLogin,
-        setUserLogged,
-        setIsLogin,
-        createUser,
-        handleChangeForm,
-        onChangePicture,
-        setImg,
-        updateProfileImg
+  const update = (e) => {
+    e.preventDefault();
+    const formDat = new FormData()
+    formDat.append('archivo', img);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      },
     }
-    
-    return(
-        <UserContext.Provider value={data} >
-            {children}
-        </UserContext.Provider>
-    )
+    const url = `https://restserver-lautaro-quevedo.herokuapp.com/api/uploads/users/${idUser}`;
+    axios.put(url, formDat)
+      .then((response) => {
+        const data = { user: {...response.data} }
+        console.log(data)
+        setUserLogged(data)
+        setRefresh(true)
+        setSomeUp(false)
+        console.log(refresh)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  const updateProfileImg = () => {
+    console.log(img)
+
+    axios.put(`https://restserver-lautaro-quevedo.herokuapp.com/api/uploads/users/${idUser}`, img)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err.response.data.errors)
+        let er = err.response.data.errors;
+        if (er.lenght === 1) {
+          alert(er)
+        } er.map(error => {
+          alert(error.msg)
+        })
+      })
+  }
+
+
+  const createUser = (event) => {
+    event.preventDefault();
+    console.log(formData)
+    axios.post('https://restserver-lautaro-quevedo.herokuapp.com/api/users', formData)
+      .then(response => {
+        const data = { ...response.data }
+        setUserLogged(data)
+        console.log(userLogged)
+        setIsLogin(true)
+        console.log(isLogin)
+      })
+      .catch(err => {
+        console.log(err.response.data.errors)
+        let er = err.response.data.errors;
+        if (er.lenght === 1) {
+          alert(er)
+        } er.map(error => {
+          alert(error.msg)
+        })
+      })
+  }
+
+
+  const data = {
+    login,
+    handleChange,
+    userLogged,
+    isLogin,
+    setUserLogged,
+    setIsLogin,
+    createUser,
+    handleChangeForm,
+    onChangePicture,
+    setImg,
+    updateProfileImg,
+    update,
+    setRefresh,
+    refresh,
+    someUp,
+  }
+
+  return (
+    <UserContext.Provider value={data} >
+      {children}
+    </UserContext.Provider>
+  )
 }
 export { UserProvider }
 export default UserContext
