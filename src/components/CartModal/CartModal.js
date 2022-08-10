b0aa0897b1496d00687adfcdad18a8781518acf6
Lautaro Quevedo <89219479+LauchaQ2@users.noreply.github.com>
2022-08-09 21:10:05 -0300

    add mercadopago

--- src/components/CartModal/CartModal.js
+++ src/components/CartModal/CartModal.js
@@ -1,10 +1,36 @@
-import React, { useContext } from 'react'
+import { faVenusMars } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import React, { useContext, useState, useEffect } from 'react'
 import UserContext from '../../context/UserContext'
 import './CartModal.css'
 
 const CartModal = ({ handleCartClose }) => {
 
     const { productCarts, totalPrice, removeItem, addItem, clearCart } = useContext(UserContext)
+    const [preferenceId, setPreferenceId] = useState()
+
+
+    const buy = async () => {
+        const response = await axios.post('http://localhost:8080/api/products/payment', productCarts)
+            .then(res => {
+                var ids = res.data.preferenceId;
+                setPreferenceId(ids)
+                console.log(ids)
+                // The source domain must be completed according to the site for which you are integrating.
+                // For example: for Argentina ".com.ar" or for Brazil ".com.br".
+                var script = document.createElement("script");
+                script.src = "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
+                script.type = "text/javascript";
+                script.setAttribute('data-preference-id', ids);;
+                document.getElementById("page-content").innerHTML = "";
+                document.querySelector("#page-content").appendChild(script);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+
 
 
     return (
@@ -13,7 +39,7 @@ const CartModal = ({ handleCartClose }) => {
                 <span>Carrito de compras</span>
                 <span className='btn-close' onClick={() => { handleCartClose() }}>X</span>
             </div>
-            <div className='body-modal'>
+            <div id='page-content' className='body-modal'>
                 {productCarts.length === 0 ? <h1>No hay productos en el carrito</h1> : <>
                     <ul className='cart-title-group'>
                         <li className='row-title'>Producto</li>
@@ -37,7 +63,11 @@ const CartModal = ({ handleCartClose }) => {
                     )
                 })}
                 <div className='footer-modal'>
-                {productCarts.length !== 0 && <p>TOTAL: {totalPrice}</p>}
+                    {productCarts.length !== 0 && <p>TOTAL: {totalPrice}</p>}
+                    <button>
+                        <a onClick={() => { buy() }}>Pagar</a>
+                    </button>
+
                 </div>
             </div>
         </div></>
